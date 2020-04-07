570a91ae5920610ca2882d26b0662249fb61f29d
AI1411 <goethe0719@gmail.com>
2020-04-07 17:47:55 +0900

    認証メールの再送処理

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -7,28 +7,47 @@
         <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
           ログイン
         </h1>
-        <form class="auth-form">
+        <form class="auth-form" @submit.prevent="submit">
+          <alert-error v-if="form.errors.has('message')" :form="form">
+            {{ form.errors.get('message') }}
+            <nuxt-link :to="{ name: 'verification.resend' }"
+            >Resend verification email</nuxt-link
+            >
+          </alert-error>
           <div class="form-group">
             <input
               type="text"
               name="email"
+              v-model="form.email"
               class="form-control form-control-lg font-14 fw-300"
+              :class="{'is-invalid': form.errors.has('email')}"
               placeholder="メールアドレス"
             />
+            <has-error :form="form" field="email"></has-error>
           </div>
           <div class="form-group">
             <input
               type="password"
               name="password"
+              v-model="form.password"
               class="form-control form-control-lg font-14 fw-300"
+              :class="{'is-invalid' : form.errors.has('password')}"
               placeholder="パスワード"
             />
+            <has-error :form="form" field="password"></has-error>
           </div>
           <div class="mt-4 mb-4 clearfix">
-            <a class="forgot-pass color-blue font-14 fw-400" href="#">パスワードを忘れた方はこちら</a>
+            <a class="forgot-pass color-blue font-14 fw-400">パスワードを忘れた方はこちら</a>
           </div>
           <div class="text-right">
-            <button type="submit" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
+            <button
+              type="submit"
+              :disabled="form.busy"
+              class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase"
+            >
+              <span v-if="form.busy">
+                <i class="fas fa-spinner fa-spin"></i>
+              </span>
               ログイン
             </button>
           </div>
@@ -44,9 +63,30 @@
 </template>
 
 <script>
-    export default {
-        name: "login"
+  export default {
+    data() {
+      return {
+        form: this.$vform({
+          email: '',
+          password: ''
+        })
+      }
+    },
+    methods: {
+      submit() {
+        this.$auth
+          .loginWith('local', {
+            data: this.form
+          })
+          .then(res => {
+            console.log(res);
+          })
+          .catch(e => {
+            this.form.errors.set(e.response.data.errors);
+          });
+      }
     }
+  }
 </script>
 
 <style scoped>
